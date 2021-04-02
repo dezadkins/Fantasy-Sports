081ef2ad9ef320ffd6d4f5b3130f71d9b5ae2af5
Dez <desmondadkins@gmail.com>
2021-04-02 19:25:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <h1>Dream Team</h1>
+      {/* header1 */}
+      {/* header2 */}
+      <div className="app__body">
+        {/* Top Section */}
+        {/* Mid-section */}
+        {/* Bottom */}
+        {/* footer */}
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9_sJCzIkqDI4tF6FozMaN7H1EDEFhA5A",
+  authDomain: "dream-teams-clone.firebaseapp.com",
+  projectId: "dream-teams-clone",
+  storageBucket: "dream-teams-clone.appspot.com",
+  messagingSenderId: "616742929602",
+  appId: "1:616742929602:web:ba15ef75940370a3c7275b",
+};
